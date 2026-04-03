c87307888a9635485e3e03a30af04e31e15d4571
gdev6145 <gdev6145@gmail.com>
2026-04-03 13:50:07 -0400

    Create initial unit tests for Spectral Cloud services

---
+++ tests/unit/unit_tests.py
@@ -0,0 +1,9 @@
+# Unit tests for Spectral Cloud services
+import unittest
+
+class TestUnitServices(unittest.TestCase):
+    def test_example(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
